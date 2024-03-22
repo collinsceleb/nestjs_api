1c84e446b7a937a2d92286b9e8d9cf470463b275
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2024-03-22 21:52:28 +0100

    Create signup endpoint and logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import * as argon2 from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
   constructor(private prismaService: PrismaService) {}
+  async signup(dto: AuthDto) {
+    try {
+      const hashedPassword = await argon2.hash(dto.password);
+      const newUser = await this.prismaService.user.create({
+        data: {
+          password: hashedPassword,
+          email: dto.email,
+        },
+      });
+      delete newUser.password;
+      return newUser;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email has been used!');
+        }
+      }
+      throw error;
+    }
+  }
   signin() {
     return {
       msg: 'I have signed in.',
-    };
-  }
-  signup() {
-    return {
-      msg: 'I have signed up.',
     };
   }
 }
