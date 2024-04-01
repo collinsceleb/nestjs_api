948e45db0dee2cdf7973157b735b552b26426b58
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2024-04-01 07:06:47 +0100

    Create sign in endpoint logic with jwt strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto/auth.dto';
 import * as argon2 from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private jwtService: JwtService,
+    private config: ConfigService,
+  ) {}
   async signup(dto: AuthDto) {
     try {
       const hashedPassword = await argon2.hash(dto.password);
@@ -16,8 +22,7 @@ export class AuthService {
           email: dto.email,
         },
       });
-      delete newUser.password;
-      return newUser;
+      return this.signToken(newUser.id, newUser.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
@@ -27,9 +32,38 @@ export class AuthService {
       throw error;
     }
   }
-  signin() {
+  async signin(dto: AuthDto) {
+    const existingUser = await this.prismaService.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!existingUser) throw new ForbiddenException('Incorrect email');
+    const comparePassword = await argon2.verify(
+      existingUser.password,
+      dto.password,
+    );
+    if (!comparePassword) {
+      throw new ForbiddenException('Incorrect Password');
+    }
+    delete existingUser.password;
+    return this.signToken(existingUser.id, existingUser.email);
+  }
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get('JWT_SECRET');
+    const token = await this.jwtService.signAsync(payload, {
+      expiresIn: '15m',
+      secret,
+    });
     return {
-      msg: 'I have signed in.',
+      access_token: token,
     };
   }
 }
